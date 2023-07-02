ed5727194ab59c54a3cb1782032c7c26fef057d8
Jose Lacueva <xkraum@gmail.com>
2023-07-02 12:40:12 +0200

    Implemented ESLint and Prettier rules.

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.scss';
+import React from 'react';
+
+const objecto = {
+  objeto: 'string',
+  objeto2: 2,
+};
+
+const funcion = (a: string) => {
+  return a;
+};
 
 function App() {
-    return <div className="App">Test</div>;
+  return <div className="App">Test</div>;
 }
 
 export default App;
